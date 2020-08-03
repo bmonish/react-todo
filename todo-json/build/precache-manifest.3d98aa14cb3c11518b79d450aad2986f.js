b872cf4ee8c3f8bff5f6ebc7ccb94d20a32df002
bmonish <monishmonib@gmail.com>
2020-08-04 00:21:50 +0530

    Move to Folder

---
+++ todo-json/build/precache-manifest.3d98aa14cb3c11518b79d450aad2986f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef684e222d4f3238e912d03fe95d25a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7a9f9cb6fb792e49bf",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "20683d9197a81052922d",
+    "url": "/static/js/2.d92b0815.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d92b0815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7a9f9cb6fb792e49bf",
+    "url": "/static/js/main.5d00ec6c.chunk.js"
+  },
+  {
+    "revision": "30d1f193c9f363536713",
+    "url": "/static/js/runtime-main.a07e7f11.js"
+  }
+]);
